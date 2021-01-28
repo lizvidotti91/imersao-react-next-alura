5008270cd647656fe67b2571df70f4a232c3bced
lizvidotti91 <liz.vidotti@gmail.com>
2021-01-28 12:53:39 -0300

    Icones

--- pages/_document.js
+++ pages/_document.js
@@ -9,8 +9,15 @@ export default class MyDocument extends Document {
     return (
       <Html>
         <Head>
+          {/* Imagem de Compartilhamento */}
+          <meta property="og:image" content="https://raw.githubusercontent.com/lizvidotti91/imersao-react-next-alura/main/src/img/img-background.png" />
+
+          <link rel="shortcut icon" href="/src/img/icon.png" />
+
           <link rel="preconnect" href="https://fonts.gstatic.com" />
           <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@100;300;400;500;700;900&display=swap" rel="stylesheet" />
+
+          <title>.:ModernQuiz:.</title>
           <body>
             <Main />
             <NextScript />
